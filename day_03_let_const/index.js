8340588fabac67bc5d6a7cbe923224c3ebe051ec
yashvi-param <yashvirwn123@gmail.com>
2025-08-18 19:26:47 +0530

    Add files via upload
    complete

---
+++ day_03_let_const/index.js
@@ -0,0 +1,12 @@
+console.log("javascript tutorial 3: var, let, const")
+
+let a = 10;
+var b = "yashvi";
+var c = null;
+const author = "yashvi";
+var d = undefined;
+{
+    let a = '20';
+    console.log(a);
+}
+console.log(a);
